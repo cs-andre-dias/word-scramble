e7a59222221153f5d076d7fb4a06c7620ac37edc
Andre Dias <andre.dias@concrete.com.br>
2016-09-30 11:56:16 -0300

    Adiciona metodo de verificacao no input do usuario

--- Word Scramble/Word Scramble/ViewController.swift
+++ Word Scramble/Word Scramble/ViewController.swift
@@ -23,20 +23,36 @@ class ViewController: UITableViewController {
     var usedWords = [String]()
     
     func isPossible(word: String) -> Bool{
+        var tempWord = title!.lowercased()
+        
+        for letter in word.characters{
+            if let pos = tempWord.range(of: String(letter)){
+                tempWord.remove(at: pos.lowerBound)
+            }else{
+                return false
+            }
+        }
         return true
     }
     
     func isOriginal(word: String) -> Bool{
-        return true
+        return !usedWords.contains(word)
     }
     
     func isReal(word: String) -> Bool{
-        return true
+        let checker = UITextChecker()
+        let range = NSMakeRange(0, word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
     }
     
     func submit(answer: String){
         
         let lowerAnswer = answer.lowercased()
+        
+        let errorTitle: String
+        let errorMessage: String
         
         if isPossible(word: lowerAnswer){
             if isOriginal(word: lowerAnswer){
@@ -46,9 +62,24 @@ class ViewController: UITableViewController {
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
                     
+                    return
+                }else{
+                    errorTitle = "Word not recognized"
+                    errorMessage = "You can't just make them up, you know!"
                 }
+            }else{
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
             }
+        }else{
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from '\(title!.lowercased())'!"
         }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    
     }
     
     func promptForUser(){
